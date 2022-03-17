5b1eaf7dca005d3e6e0cbb9f08d489b681570757
TailaiLihe <47621780+TailaiLihe@users.noreply.github.com>
2022-03-17 12:09:05 -0700

    add sound file for test7 test 10

--- test7.m
+++ test7.m
@@ -5,8 +5,10 @@ M = 100;
 K = 20;
 Q = 8;
 e = 0.01;
-[trainingSound,Fs1] = loadSound('Training_Data/'); % Use for folder
-[testingSound,Fs2] = loadSound('Test_Data/'); % Use for folder
+% [trainingSound,Fs1] = loadSound('Training_Data/'); % Use for folder
+% [testingSound,Fs2] = loadSound('Test_Data/'); % Use for folder
+[trainingSound,Fs1] = loadSound('eight/training/'); % Use for folder
+[testingSound,Fs2] = loadSound('eight/testing/'); % Use for folder
 result = predition(trainingSound,testingSound,N,M,K,Fs2,Q,e);
 function result = predition(trainingSounds,testingSounds,N,M,K,Fs,Q,distortion_eps)
     m = melfb(K, N, Fs);
@@ -30,17 +32,18 @@ function result = predition(trainingSounds,testingSounds,N,M,K,Fs,Q,distortion_e
             for i = 1:length(codebook)
                 distance(i,:) = sum((codebook{i} - codebookTest{j}).^2,2);
                 %plot(codebook{i}(:,3),codebook{i}(:,4),'o')
+                
             end
 %             plot(codebookTest{j}(:,3),codebookTest{j}(:,4),'*')
 %             hold off
-
+            
              [Min,index] = min(distance);
-             if(j == 7)
-                 distance
-                 index
-                 min
-             end
              result(j) = mode(index);
+             if j==10
+                distance
+                Min
+                index
+             end
         end 
         
         
